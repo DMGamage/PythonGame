9a18619b28d5c284d850e1f3765bbbe6c8291950
DMGamage <gamagemadhushan@gmail.com>
2023-11-10 17:46:44 +0530

    Generating the background

--- main.py
+++ main.py
@@ -2,21 +2,41 @@ import os
 import random
 import math
 import pygame
-from os import listdir
-from os.path import isfile,join
+from os.path import join
+
 pygame.init()
 
 pygame.display.set_caption("Ninja")
-BG_COLOR =(255,255,255)
-WIDTH,HEIGHT =1000,800
-FPS=60
 
-window = pygame.display.set_mode((WIDTH,HEIGHT))
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles.append(pos)
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for title in background:
+        window.blit(bg_image, title)
+    pygame.display.update()
+
 
 def main(window):
-    clock =pygame.time.Clock()
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Yellow.png")
 
-    run =True
+    run = True
     while run:
         clock.tick(FPS)
 
@@ -24,8 +44,11 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main(window)
